032adfc9c9dbe98928f126ca451288aa033d976a
jaha-sudo <romanovajaha@gmail.com>
2022-06-20 20:32:54 +0500

    navbar start

---
+++ frontend/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="w-full h-36 bg-black text-white "> 
+            Enter
+        </div>
+    );
+}
+
+export default Footer;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
-import { appWithTranslation } from "next-i18next";
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
+import { appWithTranslation } from "next-i18next";
+import Layout from "../components/Layout";
+
+const MyApp = ({ Component, pageProps, auth }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
 
 export default appWithTranslation(MyApp);
