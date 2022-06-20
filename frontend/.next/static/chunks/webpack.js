02ee1de4c75c0ed12f74ec30138b0ee8a355e88f
jaha-sudo <romanovajaha@gmail.com>
2022-06-20 18:28:04 +0500

    yarn

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46a26c4f312d81c9"; }
+/******/ 		__webpack_require__.h = function() { return "1107746947781bfa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            'darkBlue': '#344151',
+            'green':'#0bc469',
+            'white':'white',
+            'grey':'#7a7a7a',
+            'black':'black',
+            'red':'#891289'
+        },
+        screens: {
+            xsm: "0px",
+            sm: "320px",
+            // => @media (min-width: 320px) { ... }
+
+            md: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            lg: "768px",
+            // => @media (min-width: 768px) { ... }
+            xl: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            "2xl": "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "3xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
+    },
+
+    plugins: [],
+};
