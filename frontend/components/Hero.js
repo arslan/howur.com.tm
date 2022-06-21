9617e1465358235b39c4b05f4d39387f9f27aeea
jaha-sudo <romanovajaha@gmail.com>
2022-06-21 18:31:49 +0500

    add aboutSection in homePage

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="">
+    <div className="" id="home">
       <div className="w-full h-[602px]  bg-[url('/hero.png')]"></div>
       <div className="bg-red w-[2px] h-16 ml-[50.3%] -translate-y-8 flex justify-center"></div>
     </div>
