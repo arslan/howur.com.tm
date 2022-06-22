f0e9b95093c8f0d1df1eb9bfc08284d6f228cb76
jaha-sudo <romanovajaha@gmail.com>
2022-06-22 16:18:57 +0500

    some changes in productsSection

--- frontend/components/common/ServiceCard.js
+++ frontend/components/common/ServiceCard.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const ServiceCard = (props) => {
   return (
-    <div className="w-full flex my-6">
+    <div className="w-full flex h-60">
       <span className="w-1/2">
         <Image src={props.src} width={319} height={219} alt="" />
       </span>
-      <span className="w-1/2 p-6 space-y-6">
+      <span className="px-12 w-1/2 space-y-6">
         <h4 className="text-xl">{props.cardTitle}</h4>
         <p className="">
           {/* {props.cardDesc} */}
