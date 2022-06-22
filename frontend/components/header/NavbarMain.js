e01efe2d8685bc7eeb647d2bc1a2f05befa348a4
jaha-sudo <romanovajaha@gmail.com>
2022-06-22 16:49:56 +0500

    add dropdown to navbar serviceitem

--- frontend/components/header/NavbarMain.js
+++ frontend/components/header/NavbarMain.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 // import { useTranslation } from 'next-i18next'
 import { v4 as uuid_v4 } from "uuid";
+import Dropdown from "./Dropdown";
 import Lang from "./Lang";
 
 // import Lang from './Lang'
@@ -11,11 +12,11 @@ function NavbarMain(props) {
   const router = useRouter();
   const currentSection = router.asPath;
   const navigation = [
-    { name: "home", href: "#home" },
-    { name: "services", href: "#services" },
-    { name: "aboutUs", href: "#aboutUs" },
-    { name: "sertificates", href: "#sertificates" },
-    { name: "contacts", href: "#contacts" },
+    { name: "home", href: "/" },
+    { name: "services"},
+    { name: "aboutUs", href: "/#aboutUs" },
+    { name: "sertificates", href: "/#sertificates" },
+    { name: "contacts", href: "/#contacts" },
 
   ];
 
@@ -25,6 +26,9 @@ function NavbarMain(props) {
     <div {...props} className="">
       <ul className="flex justify-between mt-[5px] pr-2 mb-[6px]">
         {navigation.map(({ name, href }, i) => {
+          if (name === 'services') {
+            return <Dropdown />
+          }
           return (
               <li className="text-black text-xl w-32" key={uuid_v4()}>
                 <Link href={href} passHref>
