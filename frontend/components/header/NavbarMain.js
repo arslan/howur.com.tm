eb31343c5002b55bbb991ed99455d20faedf9420
MaxBQ <maksat.bek93@gmail.com>
2022-06-23 13:26:33 +0500

    data

--- frontend/components/header/NavbarMain.js
+++ frontend/components/header/NavbarMain.js
@@ -4,47 +4,50 @@ import { useRouter } from "next/router";
 import { v4 as uuid_v4 } from "uuid";
 import Dropdown from "./Dropdown";
 import Lang from "./Lang";
+import { useContext } from "react";
+import { layoutContext } from "../../pages/_app";
 
 // import Lang from './Lang'
 
 function NavbarMain(props) {
-  //   const { t } = useTranslation('common')
-  const router = useRouter();
-  const currentSection = router.asPath;
-  const navigation = [
-    { name: "home", href: "/" },
-    { name: "services"},
-    { name: "aboutUs", href: "/#aboutUs" },
-    { name: "sertificates", href: "/#sertificates" },
-    { name: "contacts", href: "/#contacts" },
+	//   const { t } = useTranslation('common')
+	const router = useRouter();
+	const currentSection = router.asPath;
+	const {
+		attributes: {
+			navbar: {
+				navigations: { data: navigations },
+			},
+		},
+	} = useContext(layoutContext);
 
-  ];
+	// useState for hover effect
 
-// useState for hover effect
-
-  return (
-    <div {...props} className="">
-      <ul className="flex justify-between mt-[5px] pr-2 mb-[6px]">
-        {navigation.map(({ name, href }, i) => {
-          if (name === 'services') {
-            return <Dropdown />
-          }
-          return (
-              <li className="text-black text-xl w-32" key={uuid_v4()}>
-                <Link href={href} passHref>
-                  <a
-                    className={`cursor-pointer px-4 py-2 hover:text-red ${
-                      currentSection === "/"+href ? " text-red border-t-2 border-red" : " px-4"
-                    }`}
-                  >
-                    {name}
-                  </a>
-                </Link>
-              </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div {...props} className=''>
+			<ul className='flex justify-between mt-[5px] pr-2 mb-[6px]'>
+				{navigations.map(({ attributes: { name, href } }, i) => {
+					if (href === null) {
+						return <Dropdown name={name} />;
+					}
+					return (
+						<li className='text-black text-xl w-32' key={uuid_v4()}>
+							<Link href={href} passHref>
+								<a
+									className={`cursor-pointer px-4 py-2 hover:text-red ${
+										currentSection === "/" + href
+											? " text-red border-t-2 border-red"
+											: " px-4"
+									}`}
+								>
+									{name}
+								</a>
+							</Link>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 export default NavbarMain;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,11 @@
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
-    i18n,
-    env: {
-        HCAPTCHA: process.env.HCAPTCHA,
-    },
+	i18n,
+	env: {
+		HCAPTCHA: process.env.HCAPTCHA,
+	},
+	images: {
+		domains: ["localhost"],
+	},
 };
