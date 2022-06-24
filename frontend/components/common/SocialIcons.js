9e532ea4ad970f847836001b28dd843c03dffef4
MaxBQ <maksat.bek93@gmail.com>
2022-06-24 13:51:22 +0500

    done data front & update db_dump

--- frontend/components/common/SocialIcons.js
+++ frontend/components/common/SocialIcons.js
@@ -3,23 +3,23 @@ import { FaFacebookSquare, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Socialicons() {
-  return (
-    <div className="flex flex-row">
-      <Link href="https://www.facebook.com/tylla.nal.9">
-        <a className='pr-2'>
-          <FaFacebookSquare />
-        </a>
-      </Link>
-      <Link href="https://www.instagram.com/tylla_nal/?hl=ru">
-        <a className='px-2'>
-          <FaInstagram />
-        </a>
-      </Link>
-      <Link href="https://tm.linkedin.com/company/tylla-nal-corp" >
-        <a className='pl-2'>
-          <FaLinkedinIn />
-        </a>
-      </Link>
-    </div>
-  );
+	return (
+		<div className='flex flex-row'>
+			<Link href='https://www.facebook.com/tylla.nal.9'>
+				<a className='pr-2'>
+					<FaFacebookSquare />
+				</a>
+			</Link>
+			<Link href='https://www.instagram.com/tylla_nal/?hl=ru'>
+				<a className='px-2'>
+					<FaInstagram />
+				</a>
+			</Link>
+			<Link href='https://tm.linkedin.com/company/tylla-nal-corp'>
+				<a className='pl-2'>
+					<FaLinkedinIn />
+				</a>
+			</Link>
+		</div>
+	);
 }
