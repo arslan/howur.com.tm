0a7ac7c8fd34c0e2907e26c192e61f6d43317ba5
jaha-sudo <romanovajaha@gmail.com>
2022-06-24 18:19:06 +0500

    fixed gradient bg, map, mail

--- frontend/components/ContactSection.js
+++ frontend/components/ContactSection.js
@@ -6,53 +6,55 @@ import Socialicons from "./common/SocialIcons";
 import Form from "./Form";
 
 const ContactSection = ({
-	data: {
-		formHead,
-		formName,
-		formMess,
-		formMail,
-		formButton,
-		form_number,
-		form_mail,
-		form_address,
-	},
+  data: {
+    formHead,
+    formName,
+    formMess,
+    formMail,
+    formButton,
+    form_number,
+    form_mail,
+    form_address,
+  },
 }) => {
-	return (
-		<div className='max-w-7xl mx-auto my-12' id='contacts'>
-			<h2 className='font-bold text-3xl'>{formHead}</h2>
-			<div className='w-full flex my-16'>
-				<div className='w-1/2'>
-					<Form data={{ formName, formMess, formMail, formButton }} />
-				</div>
-				<div className='w-1/2  space-y-10 pl-12 py-8 border-solid border-2 border-red'>
-					<div className='flex items-center'>
-						<HiOutlineLocationMarker />
-						<p className='text-black pl-2 pt-1'>{form_address}</p>
-					</div>
-					<div className='flex items-center'>
-						<MdOutlineEmail className='mr-2' />
-						<Link className='' href='mailto:sales@pursatlyyorish.com'>
-							<a>{form_mail}</a>
-						</Link>
-					</div>
-					<div className='flex items-center'>
-						<FiPhone />
-						<a href='tel:+99365039539' className='text-black pl-2 pt-1'>
-							{form_number}
-						</a>
-					</div>
-					<Socialicons />
-				</div>
-			</div>
-			<iframe
-				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.104460991889!2d58.38682389460728!3d37.903098546057926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f6ffd9da80a5487%3A0x27964b35d7a8cba2!2z0JTQvtC8IDY4LCA2OCwgQXNoZ2FiYXQsIFR1cmttZW5pc3Rhbg!5e0!3m2!1sen!2suk!4v1654780512502!5m2!1sen!2suk'
-				width='100%'
-				height='375px'
-				allowFullScreen=''
-				loading='lazy'
-			></iframe>
-		</div>
-	);
+  return (
+    <div className="bg-[linear-gradient(140deg,#cd320212_0%,#cd320200_41.63%)]">
+      <div className="max-w-7xl mx-auto py-12" id="contacts">
+        <h2 className="font-bold text-3xl">{formHead}</h2>
+        <div className="w-full flex my-16">
+          <div className="w-1/2">
+            <Form data={{ formName, formMess, formMail, formButton }} />
+          </div>
+          <div className="w-1/2  space-y-10 pl-12 py-8 border-solid border-2 border-red">
+            <div className="flex items-center">
+              <HiOutlineLocationMarker />
+              <p className="text-black pl-2 pt-1">{form_address}</p>
+            </div>
+            <div className="flex items-center">
+              <MdOutlineEmail className="mr-2" />
+              <Link className="" href="mailto:howur@gmail.com">
+                <a>{form_mail}</a>
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <FiPhone />
+              <a href="tel:+99365039539" className="text-black pl-2 pt-1">
+                {form_number}
+              </a>
+            </div>
+            <Socialicons />
+          </div>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3148.6718832879487!2d58.34255391571648!3d37.891355413215805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f70025ecc6a7673%3A0xf0ae0c7b777af2a4!2zQXJjaGFiaWwgSHd5IDU1LCBBc2hnYWJhdCwg0KLRg9GA0LrQvNC10L3QuNGB0YLQsNC9!5e0!3m2!1sru!2s!4v1656075780914!5m2!1sru!2s"
+          width="100%"
+          height="400px"
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default ContactSection;
--- frontend/components/SertificatesSection.js
+++ frontend/components/SertificatesSection.js
@@ -3,28 +3,30 @@ import Link from "next/link";
 import SertificatesCarousel from "./SertificatesCarousel";
 
 const SertificatesSection = ({
-	data: { headCert, underHeadCert, certificate_lists, logoCert },
+  data: { headCert, underHeadCert, certificate_lists, logoCert },
 }) => {
-	return (
-		<div className='bg-[linear-gradient(133deg,#cd320212_0%,#cd320200_41.63%)] pt-12'>
-			<div className='max-w-7xl mx-auto my-12' id='sertificates'>
-				<div className='flex flex-col items-center'>
-					<h2 className='font-bold text-3xl'>{headCert}</h2>
-					<Link href='/' passHref>
-						<a>
-							<Image
-								src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${logoCert}`}
-								width={98}
-								height={118}
-								alt=''
-							/>
-						</a>
-					</Link>
-				</div>
-				<SertificatesCarousel data={certificate_lists} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-[linear-gradient(133deg,#cd320212_0%,#cd320200_41.63%)] pt-12">
+      <div className="bg-[linear-gradient(40deg,#cd320212_0%,#cd320200_41.63%)]">
+        <div className="max-w-7xl mx-auto py-12" id="sertificates">
+          <div className="flex flex-col items-center">
+            <h2 className="font-bold text-3xl">{headCert}</h2>
+            <Link href="/" passHref>
+              <a>
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${logoCert}`}
+                  width={98}
+                  height={118}
+                  alt=""
+                />
+              </a>
+            </Link>
+          </div>
+          <SertificatesCarousel data={certificate_lists} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SertificatesSection;
