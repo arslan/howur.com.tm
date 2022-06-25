e2f0dc75064aea52b0d872b9b2bc3114b706480e
jaha-sudo <romanovajaha@gmail.com>
2022-06-26 02:48:49 +0500

    fixed 1st, 3rd grids with productsCard in homepage

--- frontend/components/products/ProductsSection.js
+++ frontend/components/products/ProductsSection.js
@@ -7,7 +7,7 @@ const ProductsSection = ({
     <>
       <div className="flex py-16 flex-col bg-[linear-gradient(228deg,#cd320212_0%,#cd320200_41.63%)]">
         <div className="max-w-7xl mx-auto">
-          <div className="w-full pb-12">
+          <div className="w-full">
             <h2 className="font-bold text-3xl">{proHeading}</h2>
           </div>
 
