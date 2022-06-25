d1da7dd5b399a020c94806d4dd957b24533cadce
jaha-sudo <romanovajaha@gmail.com>
2022-06-26 01:31:20 +0500

    fixed productCard in ProductsSection (homepage)

--- frontend/components/products/ProductCard.js
+++ frontend/components/products/ProductCard.js
@@ -1,30 +1,14 @@
 import Image from "next/image";
 
 const ProductCard = ({ src, desc }) => {
-	return (
-		<div>
-			<div className='relative overflow-hidden'>
-				<div className=''>
-					<Image
-						// objectFit="contain"
-						// layout="fill"
-						width={800}
-						height={800}
-						src={src}
-						alt=''
-					/>
-				</div>
-
-				<div className='absolute bottom-2 px-8 w-full bg-[#000]/50'>
-					<h4 className='my-2 text-xl font-semibold tracking-tight text-white'>
-						{desc}
-					</h4>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative w-72 h-96 overflow-hidden">
+      <Image src={src} layout="fill" alt="" objectFit="contain" />
+      <div class="absolute w-full h-1/6 bottom-0 inset-x-0 bg-black/50 text-white text-xl leading-4 py-4">
+        <p className="px-6">{desc} </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
-
-
