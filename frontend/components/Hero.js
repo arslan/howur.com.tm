fdf55aedae53176da9da9dca29160c023bf6b033
jaha-sudo <romanovajaha@gmail.com>
2022-06-27 04:14:50 +0500

    adaptive hero section in homepage

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = ({ img }) => {
 	return (
-		<div className='relative h-[30rem]' id='home'>
+		<div className='relative sm:h-48 md:h-64 lg:h-80 xl:h-[30rem]' id='home'>
 			<Image
 				src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${img}`}
 				layout='fill'
