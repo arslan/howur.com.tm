c7dcb7af8f1a6cfe0a7948fe116d052a58f8acbe
MaxBQ <maksat.bek93@gmail.com>
2022-06-28 12:50:07 +0500

    update strapi data & update db_dump

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-const Hero = ({ img }) => {
+const Hero = ({ data: { headVideo, head, desc } }) => {
 	return (
 		<div className='relative sm:h-48 md:h-64 lg:h-80 xl:h-[30rem]' id='home'>
 			<Image
-				src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${img}`}
+				src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${headVideo}`}
 				layout='fill'
 				alt=''
 				priority
