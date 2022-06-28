263777d2f55b589910dbc30b4fb335152efaf559
jaha-sudo <romanovajaha@gmail.com>
2022-06-28 13:39:30 +0500

    hero change

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,16 +1,26 @@
-import Image from "next/image";
-
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import styles from "../styles/Home.module.css";
 const Hero = ({ data: { headVideo, head, desc } }) => {
-	return (
-		<div className='relative sm:h-48 md:h-64 lg:h-80 xl:h-[30rem]' id='home'>
-			<Image
-				src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${headVideo}`}
-				layout='fill'
-				alt=''
-				priority
-			/>
-		</div>
-	);
+  const [isPlaying, setIsPlaying] = useState(true);
+  return (
+    <div className="max-w-7xl mx-auto py-12 2xl:px-0 sm:px-12 flex sm:flex-col sm:items-center xl:items-start sm xl:flex-row sm:space-x-0 xl:space-x-12 sm:space-y-6 xl:space-y-0">
+      <div className="sm:w-full xl:w-2/5">
+        <h1 className="w-full text-[32px] font-bold flex flex-wrap pb-8">
+          <span className=''>{head}</span>
+        </h1>
+        <p className="">{desc}</p>
+      </div>
+      <div className="sm:w-full xl:w-3/5 max-h-96  bg-red">
+        <ReactPlayer
+          url={headVideo}
+          playing={isPlaying}
+          width={"100%"}
+          height={"100%"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
