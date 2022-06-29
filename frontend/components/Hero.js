8662d141b0f713a64a8ab3ed4a845d3e293d568c
jaha-sudo <romanovajaha@gmail.com>
2022-06-29 17:48:08 +0500

    fix some element (add contact button to hero, fix address, add developed by to footer

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,12 +1,12 @@
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 import Highlighter from "react-highlight-words";
 import HighlightWords from "./HighlightWords";
 
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
-const Hero = ({ data: { headVideo, head, desc } }) => {
-
+const Hero = ({ data: { headVideo, head, desc, headButton } }) => {
   return (
     <div className="max-w-7xl mx-auto py-12 2xl:px-0 sm:px-12 flex sm:flex-col sm:items-center xl:items-start sm xl:flex-row sm:space-x-0 xl:space-x-12 sm:space-y-6 xl:space-y-0">
       <div className="sm:w-full xl:w-2/5">
@@ -14,6 +14,11 @@ const Hero = ({ data: { headVideo, head, desc } }) => {
           <HighlightWords className="opacity-10" text={head} />
         </h1>
         <p>{desc}</p>
+        <div className="sm:w-full xl:w-1/2 py-2 text-center text-white mt-8 px-2 bg-red rounded">
+          <Link href="#contacts" passhref>
+            <a className="">{headButton}</a>
+          </Link>
+        </div>
       </div>
       <div className="sm:w-full xl:w-3/5 max-h-96">
         <ReactPlayer
