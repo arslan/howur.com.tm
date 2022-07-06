06f4c7c97dbe0c4a54de2d573f510b6e99e56de0
jaha-sudo <romanovajaha@gmail.com>
2022-07-06 18:52:27 +0500

    fix hero

--- frontend/components/ServicesSection.js
+++ frontend/components/ServicesSection.js
@@ -7,13 +7,13 @@ const ServicesSection = ({
 	return (
 		<div className='bg-[linear-gradient(326.68deg,#cd320212_0%,#cd320200_41.63%)]'>
 			<div className='max-w-7xl mx-auto sm:px-12 2xl:px-0' id='services'>
-				<h2 className='font-bold xl:text-3xl lg:text-2xl sm:text-xl text-center'>{heading}</h2>
-				<div className='flex py-6'>
+				<h2 className='font-bold xl:text-3xl lg:text-2xl sm:text-xl mb-12'>{heading}</h2>
+				{/* <div className='flex py-6'>
 					<div className='w-full sm:text-center xl:text-right'>
 						<h2 className='font-bold xl:text-3xl lg:text-2xl sm:text-xl'>{under_heading}</h2>
 						<p className='sm:w-full xl:w-1/2 xl:ml-auto py-12'>{description}</p>
 					</div>
-				</div>
+				</div> */}
 				<div className='grid xl:grid-cols-2 sm:grid-cols-1 gap-6 pb-12 mx-auto relative'>
 					{service_lists.map(
 						({
